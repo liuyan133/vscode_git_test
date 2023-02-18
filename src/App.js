dde0d35002d9acd47d6d4823b03b9ca7d648a9b9
liuyan133 <1406374030@qq.com>
2023-02-19 05:36:00 +0800

    3

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       
+          <h2>
+            LY LOVE ZJQ!!
+          
+          </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
